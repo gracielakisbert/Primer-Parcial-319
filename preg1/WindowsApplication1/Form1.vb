10e45ad87821902b82b5950b43a63f686717331d
gracielakisbert <gracielaquisbert5@gmail.com>
2022-10-07 11:43:08 -0400

    Add files via upload

---
+++ preg1/WindowsApplication1/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        Dim r As Integer
+        Dim i As Integer
+        a = Convert.ToInt32(TextBox1.Text)
+        b = Convert.ToInt32(TextBox2.Text)
+        c = Convert.ToInt32(TextBox3.Text)
+
+        For i = 1 To 3
+            If a > b Then
+                r = a
+                If r > c Then
+                    r = a
+                    TextBox4.Text = TextBox4.Text + r.ToString + "-"
+                    a = 0
+                Else
+                    r = c
+                    TextBox4.Text = TextBox4.Text + r.ToString + "-"
+                    c = 0
+
+                End If
+            Else
+                r = b
+                If b > c Then
+                    r = b
+                    TextBox4.Text = TextBox4.Text + r.ToString + "-"
+                    b = 0
+
+                Else
+                    r = c
+                    TextBox4.Text = TextBox4.Text + r.ToString + "-"
+                    c = 0
+
+                End If
+            End If
+
+
+
+        Next
+
+    End Sub
+End Class
+
---
+++ preg5/ConsoleApplication2/Program.fs
@@ -0,0 +1,23 @@
+﻿// Más información acerca de F# en http://fsharp.net
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+open System
+let suma a b = a + b
+let resta a b = a - b
+let multiplicacion a b = a * b
+let division a b = a / b
+[<EntryPoint>]
+let main argumentos =
+    let val1 = Console.ReadLine()
+    let val2 = Console.ReadLine()
+    let num1 = Convert.ToInt32(val1)
+    let num2 = Convert.ToInt32(val2)
+    let res = suma num1 num2
+    let res2 = resta num1 num2
+    let res3 = multiplicacion num1 num2
+    let res4 = division num1 num2
+    Console.WriteLine("La suma de "+val1+" y "+val2+" es: "+res.ToString())
+    Console.WriteLine("La resta de "+val1+" y "+val2+" es: "+res2.ToString())
+    Console.WriteLine("La multiplicacion de "+val1+" y "+val2+" es: "+res3.ToString())
+    Console.WriteLine("La division de "+val1+" y "+val2+" es: "+res4.ToString())
+    let tecla = Console.ReadKey()
+    0 // devolver un código de salida entero
